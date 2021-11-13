7e27202f5c2843bc66aa9259246621b60679df91
Jakob Andersson <erikjakobandersson@gmail.com>
2021-11-13 11:44:38 +0100

    Updated installation of VS Code

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -14,6 +14,12 @@ sudo apt-get install fonts-powerline
 
 # Install various packages
 sudo apt update
-sudo apt install wget curl code nodejs npm docker-compose -y
 sudo apt upgrade -y
+sudo apt install software-properties-common apt-transport-https wget -y
+# Enable VS Code installation
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code -y
+
+sudo apt install curl nodejs npm docker-compose -y
 sudo apt autoremove -y
